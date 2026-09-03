26be637f7e7ab871b7c93e94e3f0a31ca0f365e9
Miiiitch77 <michelle.muiruri@strathmore.edu>
2026-09-03 10:18:19 +0300

    variabless

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,26 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    welcome()
+    variables()
+}
 
+fun welcome() {
     println("Welcome to Tenant Management System")
+}
+
+
+// Variables[Michelle]
+fun variables(){
+    println("\n=== Variables ===")
+    val tenantId = 1001
+    val name = "Jane wanjiku"
+    val phone = "0712345678"
+    val houseNumber = "A-204"
+    val monthlyRent = 25000
+    var amountPaid = 15000
+
+    println("Amount paid before: $amountPaid")
+    amountPaid = 5000
+    println("Amount paid after: $amountPaid")
 }
