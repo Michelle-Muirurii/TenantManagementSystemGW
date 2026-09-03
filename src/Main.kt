5140a423c9cc0af18318d3d6b9e6bfec4b422053
FareedKamau <fareed.kamau@strathmore.edu>
2026-09-03 15:42:29 +0300

    3rd Commit

--- src/Main.kt
+++ src/Main.kt
@@ -58,3 +58,36 @@ fun datatypes() {
     println("Rent as Double: $rentAsDouble")
     println("Registration Number: $registrationNumber")
 }
+// Strings [Fareed]
+fun string() {
+    // Task 3.1 - String Concatenation using (+) operator only
+    val name = "Jane Wanjiku"
+    val house = "A-204"
+
+    println{name + " lives in house " + house}
+
+    // Task 3.2 - String Template
+    println("$name lives in house $house")
+
+    // Task 3.3 - the Template Expression
+    val monthlyRent = 25000
+
+    println("Total Rent for 6 months: KES ${monthlyRent * 6}")
+
+    // Task 3.4 - Triple-Quoted String
+    val receipt = """
+        ===== RENT RECEIPT =====
+        
+        Tenant: Jane Wanjiku
+        House: A-204
+        Paid: KES 20000
+    """.trimIndent()
+    println(receipt)
+
+    // Task 3.5 - Predict First
+    val greeting = "Dear Tenant"
+
+    greeting.uppercase()
+    println(greeting)
+
+}
